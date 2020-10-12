8fd151980f0483c2224f431d0f7197aa32425d94
Mathura <mathuradas333@gmail.com>
2020-10-12 08:17:48 +0530

    [Mathura]Add. Ability to Analyze Mood and respond HAPPY or SAD in UC1

---
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        string moodRegex = ".*[ ][Ss][aA][dD][ ].*";
+        public string AnalyzeMood(string statement)
+        {
+            if (Regex.IsMatch(statement, moodRegex))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer");
+            Console.WriteLine("Enter your statement to analyze your mood");
+            string statement = Console.ReadLine();
+            MoodAnalyzer moodObj = new MoodAnalyzer();
+            Console.WriteLine(moodObj.AnalyzeMood(statement));
+        }
+    }
+}
