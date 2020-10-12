06cf2c56b5e5fc9923c373b90c8e3b40a1ee49fe
Mathura <mathuradas333@gmail.com>
2020-10-12 08:42:10 +0530

    AnalyzeMood passed with message given I am in Any Mood returned HAPPY in TC1.2

--- MoodAnalyzerUTest/UnitTest1.cs
+++ MoodAnalyzerUTest/UnitTest1.cs
@@ -20,5 +20,18 @@ namespace MoodAnalyzerUTest
             Assert.AreEqual(result, expectedMood);
 
         }
+        [TestMethod]
+        public void I_am_in_Any_Mood_Should_Return_HAPPY()
+        {
+            string expectedMood = "HAPPY";
+            string inputMessage = "I am in Any Mood";
+            MoodAnalyzer.MoodAnalyzerClass moodObj = new MoodAnalyzer.MoodAnalyzerClass();
+
+            //Act
+            string result = moodObj.AnalyzeMood(inputMessage);
+
+            //Assert
+            Assert.AreEqual(result, expectedMood);
+        }
     }
 }
